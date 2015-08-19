c49e84beee7a75943a8cc0408bdc26bfd2e3e873
Wyns <pauwains@gmail.com>
2015-08-19 07:58:17 +0300

    passing parameters to the yield statement

--- yield.rb
+++ yield.rb
@@ -1,14 +1,23 @@
+# def test
+#    puts "You are in the method"
+#    yield
+#    puts "You are again back to the method"
+#    yield
+# end
+# test {puts "You are in the block"}
+#
+#
+# 0utput
+# You are in the method
+# You are in the block
+# You are again back to the method
+# You are in the block
+
+# passing parameters to the yield statement
+
 def test
-   puts "You are in the method"
-   yield
-   puts "You are again back to the method"
-   yield
+   yield 5
+   puts "You are in the method test"
+   yield 100
 end
-test {puts "You are in the block"}
-
-
-0utput
-You are in the method
-You are in the block
-You are again back to the method
-You are in the block
+test {|i| puts "You are in the block #{i}"}
